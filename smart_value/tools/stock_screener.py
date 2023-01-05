732f2765496ae74e7228ee2f9c27e1be8f58cbc5
Jerry.chen <jerrychen.works@gmail.com>
2023-01-05 12:28:11 +0800

    Updated features

--- smart_value/tools/stock_screener.py
+++ smart_value/tools/stock_screener.py
@@ -99,31 +99,4 @@ def merge_data():
              'SellingGeneralAndAdministration', 'InterestExpense', 'NetIncomeCommonStockholders']].set_index('Ticker')
     df.to_csv(screener_folder / 'screener_summary.csv')
 
-# Step 2: filter
-def load_filters(csv_file):
-    """Filter the data sorted in csv based on multiple criteria.
-
-    :param csv_file: Collected data in csv format
-    :return: the list of screener result
-    """
-
-    df = pd.read_csv(csv_file)
-    # capitalization in reporting currency
-    capitalization = df['Price'] * df['Shares'] * df['Fx_rate']
-    print(capitalization)
-    total_debt = df['CurrentDebtAndCapitalLeaseObligation'] + df['LongTermDebtAndCapitalLeaseObligation']
-    print(total_debt)
-    enterprise_value = capitalization + total_debt + df['MinorityInterest'] - df['CashAndCashEquivalents']
-    # more easily realizable non-operating assets
-    monetary_assets = df['OtherShortTermInvestments'] + df['InvestmentinFinancialAssets']
-    # less easily realizable non-operating assets
-    fixed_nonop_assets = (df['InvestmentProperties'] + df['LongTermEquityInvestment']) * 0.5
-    # liquidity_coverage_ratio
-    lcr = monetary_assets / df['CurrentLiabilities']
-    current_ratio = df['CurrentAssets'] / df['CurrentLiabilities']
-
-    # filter conditions
-    condition_1 = df['CashAndCashEquivalents'] > df['CurrentDebtAndCapitalLeaseObligation']
-    condition_2 = lcr >= 1
-
-    print(df.loc[condition_1 & condition_2])
+# Step 2: filter done using ipynb with colab Notebook
